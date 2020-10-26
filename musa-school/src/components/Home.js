edbda0f46c61e789b99412ab7aebbb4f801167d7
kuiil7 <kuiil7ig11@gmail.com>
2020-10-26 01:14:44 -0700

    landing page and nav set up

---
+++ musa-school/src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../styles/Home.css'
+import main from '../images/main.jpg'
+
+
+
+
+
+function Home () {
+  return (
+
+
+ 
+
+<div id="home1"  >
+  
+<h1 id="home3" >how do you write on center?</h1>
+
+   
+
+    </div>
+
+
+  
+  );
+}
+
+export default Home;
